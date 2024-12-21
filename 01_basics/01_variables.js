c7cfb16e1697afc11878d8cac790fa61ec7d09b8
singhavani26 <88276952+singhavani26@users.noreply.github.com>
2024-12-21 18:12:52 +0000

    commit datatypes changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14553
+let accountEmail = "avani@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+console.log(accountId);
+accountId = 5
+console.table([accountEmail, accountId, accountPass])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // in node, there is different syntax for alert // this wors on browser
+let name ="Avani"
+let age = 13
+let isLoggedIn = false
+// number => 2 to power 53
+//nigint
+// string =""
+//boolean == true/false
+// null = standalone value
+//umdefined, variable declared but no value
+//symbol (used in react to find uniqueness)
+
+// object
+console.log(typeof "avani");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
