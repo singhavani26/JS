b94f4e4b37fe0903ea5021d95f4394151e4c98fc
singhavani26 <88276952+singhavani26@users.noreply.github.com>
2024-12-22 06:15:34 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "avani"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0
+
+let loggedIn = "av78"
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false
+// "hitesh" = true
